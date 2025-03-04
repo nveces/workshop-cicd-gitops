bd736d4a3d590d579da41c3c82434124fc41215d
Nveces <nveces@gmail.com>
2025-03-04 13:58:01 +0100

    add workflow_dispatch evento to execute manually the gh action

--- scripts/setup_lab_multi.sh
+++ scripts/setup_lab_multi.sh
@@ -3,31 +3,34 @@
 ##
 
 ##
-# Users 
+# Users
 ##
-USERS="user01
-user02
-user03
-user04
-user05
-user06
-user07
-user08
-user09
-user10
-user11
-user12
-user13
-user14
-user15
-"
+
+num_users=25
+
+# New USERS array
+USERS=()
+
+# populating USERS array
+for ((i=1; i<=num_users; i++)); do
+  user_name=$(printf "user%02d" $i)
+  USERS+=("$user_name")
+done
+
+# checking USERS array
+for i in ${USERS[@]}
+do
+  echo "We will use: htpasswd -b users.htpasswd $i $i"
+done
+
+
 GITOPS_NS="openshift-gitops"
 
 ##
 # Adding user to htpasswd
 ##
 htpasswd -c -b users.htpasswd admin password
-for i in $USERS
+for i in ${USERS[@]}
 do
   htpasswd -b users.htpasswd $i $i
 done
@@ -59,7 +62,7 @@ EOF
 cat oauth.yaml | oc apply -f -
 
 ##
-# Disable self namespaces provisioner 
+# Disable self namespaces provisioner
 ##
 oc patch clusterrolebinding.rbac self-provisioners -p '{"subjects": null}'
 
@@ -68,24 +71,24 @@ oc patch clusterrolebinding.rbac self-provisioners -p '{"subjects": null}'
 ##
 oc adm policy add-cluster-role-to-user admin admin
 
-## 
+##
 # Create Cluster Roles for Tekton (Manage triggers and get secret content in all namespaces)
 ##
 oc apply -f ./scripts/files/tekton_cluster_roles.yaml
 
-## 
+##
 # Install Pipelines Operator
 ##
 oc apply -f ./scripts/files/redhat_pipelines.yaml
 sleep 60
 
-## 
+##
 # Install GitOps Operator
 ##
 oc apply -f ./scripts/files/redhat_gitops.yaml
 sleep 60
 
-for i in $USERS
+for i in ${USERS[@]}
 do
 
   ##
@@ -95,12 +98,12 @@ do
   oc label namespace $i-jump-app-dev argocd.argoproj.io/managed-by=$i-gitops-argocd --overwrite
   oc adm policy add-role-to-user admin $i -n $i-jump-app-dev
   oc adm policy add-role-to-user admin system:serviceaccount:$i-gitops-argocd:argocd-argocd-application-controller -n $i-jump-app-dev
-  
+
   oc new-project $i-jump-app-dev-k8s
   oc label namespace $i-jump-app-dev-k8s argocd.argoproj.io/managed-by=$i-gitops-argocd --overwrite
   oc adm policy add-role-to-user admin $i -n $i-jump-app-dev-k8s
   oc adm policy add-role-to-user admin system:serviceaccount:$i-gitops-argocd:argocd-argocd-application-controller -n $i-jump-app-dev-k8s
-  
+
   oc new-project $i-jump-app-dev-helm
   oc label namespace $i-jump-app-dev-helm argocd.argoproj.io/managed-by=$i-gitops-argocd --overwrite
   oc adm policy add-role-to-user admin $i -n $i-jump-app-dev-helm
@@ -115,7 +118,7 @@ do
   oc label namespace $i-jump-app-pro argocd.argoproj.io/managed-by=$i-gitops-argocd --overwrite
   oc adm policy add-role-to-user admin $i -n $i-jump-app-pro
   oc adm policy add-role-to-user admin system:serviceaccount:$i-gitops-argocd:argocd-argocd-application-controller -n $i-jump-app-pro
-  
+
   oc new-project $i-jump-app-cicd
   oc label namespace $i-jump-app-cicd argocd.argoproj.io/managed-by=$i-gitops-argocd --overwrite
   oc adm policy add-role-to-user admin $i -n $i-jump-app-cicd
@@ -131,7 +134,7 @@ do
 
   ##
   # Create ArgoCD Roles and Rolebindings
-  ## 
+  ##
   # oc process -f ./scripts/files/argocd_objs.yaml -p USERNAME=$i | oc apply -f -
 
   ##
@@ -140,21 +143,25 @@ do
   # oc process -f ./scripts/files/argocd_operator.yaml -p USERNAME=$i | oc apply -f -
   # sleep 30
 
-  ## 
+  ##
   # Install ArgoCD
   ##
   oc apply -f ./scripts/files/argocd.yaml -n $i-gitops-argocd
 
-  ## 
+  ##
   # Install ArgoCD Project
   ##
-  oc process -f ./scripts/files/argocd_project.yaml -p USERNAME=$i | oc apply -f - 
+  oc process -f ./scripts/files/argocd_project.yaml -p USERNAME=$i | oc apply -f -
 
 done
 
-for i in $USERS
+for i in ${USERS[@]}
 do
   GITOPS_NS="${GITOPS_NS},${i}-gitops-argocd"
 done
 
 oc patch subscription openshift-gitops-operator -n openshift-operators -p '{"spec":{"config":{"env":[{"name":"ARGOCD_CLUSTER_CONFIG_NAMESPACES","value":"'${GITOPS_NS}'"}]}}}' --type=merge
+
+exit 0
+
+# EOF
